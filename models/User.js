9c2fc1bac5b4c6d3e65b9099b728ff5a9619ee68
Alex Purfield <alexpurfield@gmail.com>
2023-10-17 22:26:16 -0600

    finished User model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const {Schema, model} = require('mongoose');
+//destructure object
+
+const userSchema= new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ //regex for validating email!
+
+        },
+        thoughts: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+
+        friends: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+
+    });
+
+    userSchema.virtual('friendCount').get(function () {
+        return this.friends.length;
+    });
+    const User= mongoose.model('User', userSchema);
+    module.exports= User;
